68aed33aebd8f8abffbcbf2fa9168e53a1a60db9
Philip <phil.leechin@outlook.com>
2022-03-12 18:54:05 -0500

    update folder name

--- src/Container/Beerindex.js
+++ src/Container/Beerindex.js
@@ -1,5 +1,15 @@
+import { useEffect } from "react"
+import { getBeers } from '../action/actionBrewer'
+import { connect } from 'react-redux'
+
 function BeerIndex(props){
+
+    useEffect (() => props.getBeers())
     return<h1>List your beers!</h1>
 }
 
-export default BeerIndex
+const mapStateToProps = (state) => {
+    return {beers: state.beers}
+}
+
+export default connect(null, { getBeers}) (BeerIndex)
