88b60ce5354e2b1aa2f9802e8519856b54bdddc8
Philip <phil.leechin@outlook.com>
2022-03-12 18:54:35 -0500

    add in redux-thun, and connect

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux';
 import reducer from './Redux/reducer';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <React.StrictMode>
