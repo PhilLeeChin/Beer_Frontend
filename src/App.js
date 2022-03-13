99be07abec8b65c9d7846c02a1410791010f4bc1
Philip <phil.leechin@outlook.com>
2022-03-13 00:36:19 -0500

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { BeerList, BeerShow } from './components';
+import { BeerList, BeerShow, Bar } from './components/imports';
 import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <h1>What's Your Beer?</h1>
+      <Bar/>
       <Switch>
         <Route path="/beers/:id"><BeerShow/></Route>
         <Route path="/beers/"><BeerList/></Route>
