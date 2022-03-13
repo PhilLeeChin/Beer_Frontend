1b7ba57cfddcd77c5ff8173eac2d304b44ba247d
Philip <phil.leechin@outlook.com>
2022-03-13 00:41:01 -0500

    update to grab beer info

--- src/components/Beershow.js
+++ src/components/Beershow.js
@@ -1,6 +1,26 @@
-function BeerShow(props){
+import React, { useEffect } from "react"
+import { useParams } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { getBeer } from "../actions/actionBrewer"
 
-    return <h1>Here are your Beers.</h1>
+
+function BeerShow({getBeer, name, description, country, alcoholLvl,yrMade}){
+    const routeId = useParams().id
+    useEffect(() => {
+        getBeer(routeId)
+    }, [getBeer, routeId])
+
+    return <div className="self">
+        <h3>{name}</h3> 
+        <p>{description}</p>
+        <p>{country}</p>
+        <p>{alcoholLvl}</p>
+        <p>{yrMade}</p>
+    </div>
 }
 
-export default BeerShow
+const mapStateToProps = (state) => {
+    return {...state.grabBeer}
+}
+
+export default connect(mapStateToProps, {getBeer})(BeerShow)
