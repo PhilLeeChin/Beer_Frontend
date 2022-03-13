b2fbcf566fe44f5c66851dc499c47460c48d72fd
Philip <phil.leechin@outlook.com>
2022-03-13 00:38:22 -0500

    add get beer case

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,11 +1,20 @@
 const initialState = {
-    beers: []
+    beers: [],
+    grabBeer: {
+        name: "",
+        description: "",
+        alcoholLvl: 0,
+        country: "",
+        yrMade: 0,
+    }
 }
 
-export default function reducer(state=initialState, action){
+export function reducer(state=initialState, action){
     switch (action.type){
         case "GET_BEERS":
             return {...state, beers: action.payload}
+        case "GET_BEER":
+            return {...state, grabBeer: action.payload}
         default:
             return {...state}
     }
