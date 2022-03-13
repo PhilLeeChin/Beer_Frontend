f734a66a27031f7f3604fcbe8763ecf7943fa11f
Philip <phil.leechin@outlook.com>
2022-03-13 00:41:52 -0500

    update naming convention, add Bar(nav bar)

--- src/components/imports.js
+++ src/components/imports.js
@@ -1,3 +1,4 @@
-export { default as BeerCard } from './BeersCard';
+export { default as BeerCard } from './Beercard';
 export { default as BeerShow } from './Beershow';
 export { default as BeerList } from '../containers/Beerlist';
+export { default as Bar } from './Bar';
