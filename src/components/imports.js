045a51a4e5c8e1e7632c89d9ff191c02d762d97d
Philip <phil.leechin@outlook.com>
2022-03-15 19:56:26 -0400

    add verify component

--- src/components/imports.js
+++ src/components/imports.js
@@ -1,4 +1,5 @@
 export { default as BeerCard } from './Beercard';
 export { default as BeerShow } from './Beershow';
 export { default as BeerList } from '../containers/Beerlist';
+export { default as Verify } from './Verify';
 export { default as Bar } from './Bar';
