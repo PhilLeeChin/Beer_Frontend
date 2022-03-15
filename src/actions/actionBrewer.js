9ac9b649fdeff9805e9106a6ee7794379ce417d9
Philip <phil.leechin@outlook.com>
2022-03-15 19:55:47 -0400

    add submit for login a register a user

--- src/actions/actionBrewer.js
+++ src/actions/actionBrewer.js
@@ -9,3 +9,27 @@ export const getBeer = (id) => {
     .then(res => res.json())
     .then(beer => dispatch({type: "GET_BEER", payload: beer}))
 }
+
+export const submitRegister = (user) => {
+    return dispatch => fetch("http://localhost:3001/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+    .then(res => res.json())
+    .then
+}
+
+export const submitLogin = (user) => {
+    return dispatch => fetch("http://localhost:3001/sessions", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+    .then(res => res.json())
+    .then
+}
